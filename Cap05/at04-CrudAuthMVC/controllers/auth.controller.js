e526b76b436078e07a2b8867e460f6e36276f591
MIguel Gustavo De Souza <miguelg.souza01@gmail.com>
2026-09-10 20:05:16 -0300

    Commit para aprendizado

---
+++ Cap05/at04-CrudAuthMVC/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+import bcrypt from 'bcryptjs'
+import pool from '../config/database.js'
+import jwt from 'jsonwebtoken'
+
+export const gerarHashSenha = async (senha)=> bcrypt.hash(senha, 10)
+export const compararSenha = async (senha, hash)=> {bcrypt.compare(senha, hash)}
+
+export const gerarToken = (payload) => jwt.sign(
+    payload,
+    process.env.JWT_SECRET,
+    {expiresIn: '8h'}
+)
